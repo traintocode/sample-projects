141cace07f4ea9c05bcfbf460b4fa3b13e7ace75
James Charlesworth <jcharlesworthuk@gmail.com>
2023-11-29 22:02:13 +0000

    Added a sample openai call to the lambda

--- ai-apps/debug.ts
+++ ai-apps/debug.ts
@@ -1,11 +1,9 @@
 import { APIGatewayEvent, Context, SNSEvent } from "aws-lambda";
 import { main } from "./handler.js";
 
-debugger;
 main({
     body: JSON.stringify({ subject: 'trains' })
-} as APIGatewayEvent, {} as Context)
-.catch(e => {
+} as APIGatewayEvent, {} as Context).catch(e => {
     console.error(e);
     debugger;
 })
