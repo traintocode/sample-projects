73cba7808dfa135bc25d765896444533d6f102c7
James Charlesworth <jcharlesworthuk@gmail.com>
2023-12-02 22:38:40 +0000

    Added backend for poetry generator

--- ai-apps/backend/handler.ts
+++ ai-apps/backend/handler.ts
@@ -4,77 +4,41 @@ import OpenAI from "openai";
 const env = <{ OPENAI_KEY: string }>process.env;
 
 type RequestBody = {
-    textToCheck: string
+    subject: string,
+    temperature?: number,
+    model?: "gpt-3.5-turbo" | "gpt-4"
 };
-
-type ReplacementsArgType = {
-    changeFrom: string,
-    changeTo: string,
-    reason: "Grammar" | "Spelling"
-}[]
 
 export async function main(event: APIGatewayEvent, context: Context) {
     const body = <RequestBody>JSON.parse(event.body!);
-    if (!body?.textToCheck?.length) return { 
+    if (!body?.subject?.length) return { 
         statusCode: 400, 
         body: JSON.stringify({ message: 'Invalid request' })
     };
     const openai = new OpenAI({ apiKey: env.OPENAI_KEY });
 
-    const prompt = "Correct the spelling and grammatical errors in the following text:\n\n";
+    const poemPrompt = `Write me a poem about ${body.subject}.  
+    The poem should be cheerful and include rhyme, alliteration, metaphors, or similes to add depth to the poem.
+    Keep it to about 100 words.
+    Respond with just the poem.`
 
     const gptResponse = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: body.model || "gpt-3.5-turbo",
+        temperature: body.temperature,
         messages: [
             {
                 role: "user",
-                content: prompt + body.textToCheck
+                content: poemPrompt
             }
-        ],
-        functions: [
-            {
-                name: "makeCorrections",
-                description: "Makes spelling or grammar corrections to a body of text",
-                parameters: {
-                    type: "object",
-                    properties: {
-                        replacements: {
-                            type: "array",
-                            description: "Array of corrections",
-                            items: {
-                                type: "object",
-                                properties: {
-                                    changeFrom: {
-                                        type: "string",
-                                        description: "The word or phrase to change"
-                                    },
-                                    changeTo: {
-                                        type: "string",
-                                        description: "The new word or phrase to replace it with"
-                                    },
-                                    reason: {
-                                        type: "string",
-                                        description: "The reason this change is being made",
-                                        enum: ["Grammar", "Spelling"]
-                                    }                                    
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        ],
-        function_call: { name: 'makeCorrections' }
+        ]
     });
 
-    const args = <{ replacements: ReplacementsArgType[] }>JSON.parse(
-            gptResponse.choices[0].message.function_call!.arguments);
+    const result = gptResponse.choices[0].message.content;
 
     return {
         statusCode: 200,
         headers: {
-            ["Access-Control-Allow-Origin"]: "http://localhost:5173"
         },
-        body: JSON.stringify(args.replacements)
+        body: result
     };
 }
