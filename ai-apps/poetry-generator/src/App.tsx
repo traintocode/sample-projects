1505fdaeb62b3556ebd4b67bf8efb27065ca2963
James Charlesworth <jcharlesworthuk@gmail.com>
2023-12-03 14:50:23 +0000

    Added AI poetry sample project

--- ai-apps/poetry-generator/src/App.tsx
+++ ai-apps/poetry-generator/src/App.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 
+type RequestBody = {
+  subject: string,
+  temperature?: number,
+  model?: "gpt-3.5-turbo" | "gpt-4"
+};
+
 function App() {
   const [ subject, setSubject ] = useState('');
   const [ creativity, setCreativity ] = useState(80);
   const [ isBusy, setIsBusy ] = useState(false);
+  const [ poem, setPoem ] = useState('');
 
   const submitForm: React.FormEventHandler = async (e) => {
     e.preventDefault();
     setIsBusy(true);
-    // send to backend...
+    const body: RequestBody = {
+      subject,
+      temperature: creativity / 100,
+      model: creativity > 0.8 ? 'gpt-4' : 'gpt-3.5-turbo'
+    };
+    const response = await fetch('https://ibk7ftff62hk5yg4rqdmksui340vbwhp.lambda-url.eu-west-2.on.aws/', {
+      method: 'POST',
+      body: JSON.stringify(body)
+    });
+    setPoem(await response.text());
     setIsBusy(false);
   }
 
@@ -37,7 +53,9 @@ function App() {
         />
       <button type="submit" disabled={isBusy}>Create</button>
     </form>
-
+    <pre className="poem">
+      {poem}
+    </pre>
   </main>
 }
 
